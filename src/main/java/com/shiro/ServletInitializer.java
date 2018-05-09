6cf0fccdef5328cd4d6b32eddfb79355c9ee6dbe
minghaozhi <597575122@qq.com>
2018-05-09 17:36:48 +0800

    springboot-shiro

--- src/main/java/com/shiro/ServletInitializer.java
+++ src/main/java/com/shiro/ServletInitializer.java
@@ -3,6 +3,9 @@ package com.shiro;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * @author Administrator
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
